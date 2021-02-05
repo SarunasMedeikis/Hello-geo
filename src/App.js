95545abb15b72247994795bfd40d97b278a5c64b
Sarunas Medeikis <SarunasMedeikis@users.noreply.github.com>
2021-02-05 19:00:56 +0200

    Added functionality with mock login, checking that login is not empty, getting IP address via API and getting native Greeting via API.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import "./App.css";
 import React from "react";
 
-function App() {
-  const [info, setInfo] = React.useState({
-    ip: "",
-    langauge: "",
-    greeting: "",
-  });
-
+function Dashboard({ info, setInfo }) {
   function getIpAddress() {
     const urlString = "https://api.ipify.org?format=json";
     fetch(urlString)
       .then((res) => res.json())
       .then((data) => {
-        setInfo({ ...info, ip: data.ip });
+        return setInfo({ ...info, ip: data.ip });
       })
       .catch((e) => console.error("Error = = = ", e));
   }
@@ -27,14 +21,91 @@ function App() {
 
   React.useEffect(() => {
     getIpAddress();
-  }, []);
+  }, [info.userLoggedin]);
+
+  React.useEffect(() => {
+    getNativeGreeting();
+  }, [info.ip]);
+
+  return (
+    <div className="dashboard">
+      <h1>
+        {info.greeting} {info.userName}, you have succesfully logged in!
+      </h1>
+      <button
+        type="button"
+        onClick={() =>
+          setInfo({
+            ip: "",
+            greeting: "",
+            userLoggedin: false,
+            userName: "",
+            password: "",
+          })
+        }
+      >
+        Log out
+      </button>
+    </div>
+  );
+}
+
+function Login({ info, setInfo }) {
+  const [canRegister, setCanRegister] = React.useState(false);
+
+  React.useEffect(() => {
+    if (info.userName.length > 0 && info.password.length > 0) {
+      setCanRegister(true);
+    }
+  }, [info.userName, info.password]);
+
+  function onChange(event) {
+    return setInfo({ ...info, [event.target.name]: event.target.value });
+  }
+
+  return (
+    <div className="login">
+      <h1>Please login</h1>
+      <form onSubmit={() => setInfo({ ...info, userLoggedin: true })}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={info.userName}
+          onChange={onChange}
+          name="userName"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          value={info.password}
+          onChange={onChange}
+          name="password"
+        />
+        <button type="submit" disabled={!canRegister}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+function App() {
+  const [info, setInfo] = React.useState({
+    userLoggedin: false,
+    ip: "",
+    greeting: "",
+    userName: "",
+    password: "",
+  });
 
   return (
     <div className="App">
       <div className="App-header">
-        <h1>Hello from!</h1>
-        <p>{info.ip}</p>
-        <p>{info.greeting}</p>
+        {info.userLoggedin ? (
+          <Dashboard info={info} setInfo={setInfo} />
+        ) : (
+          <Login info={info} setInfo={setInfo} />
+        )}
       </div>
     </div>
   );
