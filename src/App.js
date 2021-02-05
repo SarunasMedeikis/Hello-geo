acfe900a5990835353e0da5945ec6d18db2a3a18
Sarunas Medeikis <SarunasMedeikis@users.noreply.github.com>
2021-02-05 16:13:23 +0200

    Working, however, need to add mock log in, and pass it to useEfect, to only run once every login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [info, setInfo] = React.useState({
+    ip: "",
+    langauge: "",
+    greeting: "",
+  });
+
+  function getIpAddress() {
+    const urlString = "https://api.ipify.org?format=json";
+    fetch(urlString)
+      .then((res) => res.json())
+      .then((data) => {
+        setInfo({ ...info, ip: data.ip });
+      })
+      .catch((e) => console.error("Error = = = ", e));
+  }
+
+  function getNativeGreeting() {
+    const urlString = `https://fourtonfish.com/hellosalut/?ip=${info.ip}`;
+    fetch(urlString)
+      .then((res) => res.json())
+      .then((data) => setInfo({ ...info, greeting: data.hello }));
+  }
+
+  React.useEffect(() => {
+    getIpAddress();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>Hello from!</h1>
+        <p>{info.ip}</p>
+        <p>{info.greeting}</p>
+      </div>
     </div>
   );
 }
